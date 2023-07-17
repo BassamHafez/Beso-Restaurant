b830aae2a12e2c9586b81b8fdf7ec3a47bbc3024
Bassam Hafez <bassamhafez790@gmail.com>
2023-07-18 02:01:01 +0300

    change meal

--- js/about.js
+++ js/about.js
@@ -24,46 +24,46 @@ document.onreadystatechange = function() {
     }
 }
 
-homeLink.addEventListener("click",function(){
-    footer.classList.remove=("d-none");
-    details.classList.remove("d-none"); 
-    rows.classList.add("d-none");
+    homeLink.addEventListener("click",function(){
+        footer.classList.remove=("d-none");
+        details.classList.remove("d-none"); 
+        rows.classList.add("d-none");
+        for(var i=0;i<navLink.length;i++){
+            navLink[i].classList.remove("active");
+        }
+        this.classList.add("active");
+    })
+
+
     for(var i=0;i<navLink.length;i++){
-        navLink[i].classList.remove("active");
-    }
+    navLink[i].addEventListener("click",function(e){
+    currentMeal=e.target.text;
+    footer.classList.add=("d-none");
+    details.classList.add("d-none");
+    rows.classList.remove("d-none");
     this.classList.add("active");
-})
+    homeLink.classList.remove("active");
+    getRecipes(currentMeal);
+    })
+    }
 
 
-for(var i=0;i<navLink.length;i++){
-navLink[i].addEventListener("click",function(e){
-currentMeal=e.target.text;
-footer.classList.add=("d-none");
-details.classList.add("d-none");
-rows.classList.remove("d-none");
-this.classList.add("active");
-homeLink.classList.remove("active");
-getRecipes(currentMeal);
-})
-}
+    async function getRecipes(type) {
+        loadScreen.style.display = "flex";
+        document.getElementById("body").style.cssText="overflow-y:hidden";
+        var response =await fetch( `https://forkify-api.herokuapp.com/api/search?q=${type}`);
+        recipes=(await response.json()).recipes;
+        display();
+        // var httpRequest = new XMLHttpRequest();
+        // httpRequest.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${type}`);
+        // httpRequest.send();
+        // httpRequest.addEventListener("readystatechange", function () {
+        //     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        //         recipes = JSON.parse(httpRequest.response).recipes;
+        //         display();
 
-
-async function getRecipes(type) {
-    loadScreen.style.display = "flex";
-    document.getElementById("body").style.cssText="overflow-y:hidden";
-    var response =await fetch( `https://forkify-api.herokuapp.com/api/search?q=${type}`);
-    recipes=(await response.json()).recipes;
-    display();
-    // var httpRequest = new XMLHttpRequest();
-    // httpRequest.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${type}`);
-    // httpRequest.send();
-    // httpRequest.addEventListener("readystatechange", function () {
-    //     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-    //         recipes = JSON.parse(httpRequest.response).recipes;
-    //         display();
-
-    //     }
-    // })
+        //     }
+        // })
 }
 
 function display() {
@@ -71,15 +71,17 @@ function display() {
     for (var i = 0; i < recipes.length; i++) {
         recipe += `
    
-        <div class=" col-sm-6 col-md-4 col-lg-3 ">  
+        <div class=" col-sm-6 col-md-4">  
         <div class="meal">
         <div class="img-meal">
           <img src="${recipes[i].image_url}" class="w-100 meal-img" alt="meal">
           
            <div class="recipe-title p-3">
             <p class="title-p">${recipes[i].title}</p>
-            <a href=${recipes[i].source_url} target="_blank" class="btn btn-outline-warning my-1 ">Source</a>
-            <a href=# onclick="getDetails(${recipes[i].recipe_id})" class="btn btn-outline-warning my-1 " data-bs-toggle="modal" data-bs-target="#exampleModal">Details</a>
+             <div>
+                <a href=${recipes[i].source_url} target="_blank" class="btn my-1 ">Source</a>
+                <a href=# onclick="getDetails(${recipes[i].recipe_id})" class="btn my-1 " data-bs-toggle="modal" data-bs-target="#exampleModal">Details</a>
+              </div>
             </div>
            </div>
         </div>
@@ -87,16 +89,16 @@ function display() {
         `
     }
 
-rows.innerHTML=recipe;
-loadScreen.style.display = "none";
-document.getElementById("body").style.cssText="overflow-y:auto";
-}
+    rows.innerHTML=recipe;
+    loadScreen.style.display = "none";
+    document.getElementById("body").style.cssText="overflow-y:auto";
+    }
 
-async function getDetails(Id){
-var response=await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${Id}`);
-detailsArray=(await response.json()).recipe;
-displayDetails();
-}
+    async function getDetails(Id){
+    var response=await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${Id}`);
+    detailsArray=(await response.json()).recipe;
+    displayDetails();
+    }
 
 function displayDetails(){
     var detailsCartoona="";
@@ -129,10 +131,11 @@ function seperateIngredients(){
 
 window.addEventListener("scroll",getReveal);
 
+let windoHeight=window.innerHeight;
+const revealPoint=150;
+
 function getReveal(){
     for(let i=0;i<reveals.length;i++){
-        let windoHeight=window.innerHeight;
-        const revealPoint=150;
         let distanceTop=reveals[i].getBoundingClientRect().top;
         if(distanceTop<windoHeight-revealPoint){
             reveals[i].classList.add("showing");
